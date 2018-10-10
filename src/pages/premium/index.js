d50b496c5d3e27485e0434fe96bf32176bcad1d6
Joakim Sjöbäck <joakim.sjoback@hotmail.com>
2018-10-10 12:19:49 +0200

    final css tuning

--- src/pages/premium/index.js
+++ src/pages/premium/index.js
@@ -5,15 +5,18 @@ import Filterlogo from '../../assets/images/filtervit.png';
 import Search from '../../assets/images/icons/search.png';
 import PremiumInfoFooter from '../../components/premium-info-footer/index';
 import Burger from '../../components/header/menu-burger/index';
-import Logo from '../../components/logo/index';
+import Logo from '../../assets/images/icons/logo.png';
+
+import { NavLink } from 'react-router-dom'
+
 
 class Premium extends Component {
   render(){
     return (
       <div>
     <Burger />
-    <Logo />
-
+   <div className="fixed"></div>
+   <NavLink to="/"><img src={Logo} className="filterlogo" alt="" /></NavLink>
     <h1>Varför skaffa premium?</h1>
 <PremiumFeatures />
 <PremiumInfoFooter />
